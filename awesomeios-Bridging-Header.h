da6d86e88e2c1444e136fefd0d5a259d153c2a7c
oncemore <oncemoreforyou@outlook.com>
2017-07-27 02:56:07 +0900

    add new bridging header file
    add new bridginf header file

---
+++ awesomeios-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  awesomeios-Bridging-Header.h
+//  awesomeios
+//
+//  Created by Andrey on 7/27/17.
+//  Copyright © 2017 Leor Benari. All rights reserved.
+//
+
+#ifndef awesomeios_Bridging_Header_h
+#define awesomeios_Bridging_Header_h
+
+#import "NirKxMenu.h"
+
+#endif /* awesomeios_Bridging_Header_h */
